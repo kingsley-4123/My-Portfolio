2cbaea50d236e44e68248ca3c7ab3ff12b67f790
kingsley-4123 <ekenebtcboy@gmail.com>
2025-11-09 15:38:49 +1100

    second commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,9 @@ function Navbar(){
         <nav className="w-full py-4 px-6 flex justify-between items-center bg-transparent">
             <div className="text-white font-semibold">Dollar Sid</div>
             <div className="flex gap-4 items-center">
-                <a href="#projects" className="text-sm text-gray-200 hover:text-white">Projects</a>
-                <a href="#about" className="text-sm text-gray-200 hover:text-white">About</a>
-                <a href="#contact" className="text-sm text-gray-200 hover:text-white">Contact</a>
+                <a href="#projects" className="text-sm bg-[#04132e] hover:bg-cyan-300 px-3 py-1 rounded-md text-gray-200 hover:text-[#04132e]">Projects</a>
+                <a href="#about" className="text-sm bg-[#04132e] hover:bg-cyan-300 px-3 py-1 rounded-md text-gray-200 hover:text-[#04132e]">About</a>
+                <a href="#contact" className="text-sm bg-[#04132e] hover:bg-cyan-300 px-3 py-1 rounded-md text-gray-200 hover:text-[#04132e]">Contact</a>
             </div>
         </nav>
     )
