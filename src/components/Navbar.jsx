b6e15f5f7b166bd11ca5ac3a542c0b213cc0a801
kingsley-4123 <ekenebtcboy@gmail.com>
2025-11-09 16:58:50 +1100

    third commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,8 @@
 function Navbar(){
     return (
-        <nav className="w-full py-4 px-6 flex justify-between items-center bg-transparent">
+        <nav className="w-full py-4 px-4 flex justify-between items-center bg-transparent">
             <div className="text-white font-semibold">Dollar Sid</div>
-            <div className="flex gap-4 items-center">
+            <div className="ml-3 flex gap-2 items-center">
                 <a href="#projects" className="text-sm bg-[#04132e] hover:bg-cyan-300 px-3 py-1 rounded-md text-gray-200 hover:text-[#04132e]">Projects</a>
                 <a href="#about" className="text-sm bg-[#04132e] hover:bg-cyan-300 px-3 py-1 rounded-md text-gray-200 hover:text-[#04132e]">About</a>
                 <a href="#contact" className="text-sm bg-[#04132e] hover:bg-cyan-300 px-3 py-1 rounded-md text-gray-200 hover:text-[#04132e]">Contact</a>
